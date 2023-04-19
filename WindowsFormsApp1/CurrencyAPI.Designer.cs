6078bcef45390ba32e3bd8ee35bdf5f2f32a9653
kwiacek175 <259378@student.pwr.edu.pl>
2023-04-19 06:28:10 +0200

    Modyfikacja

--- WindowsFormsApp1/CurrencyAPI.Designer.cs
+++ WindowsFormsApp1/CurrencyAPI.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class StudentAPI
+    partial class CurrencyAPI
     {
         /// <summary>
         /// Required designer variable.
@@ -35,77 +35,98 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(36, 121);
+            this.button1.Location = new System.Drawing.Point(24, 47);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 74);
+            this.button1.Size = new System.Drawing.Size(78, 48);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Uzyskać kurs waluty";
+            this.button1.Text = "Uzyskaj kurs waluty";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(45, 323);
+            this.listBox1.Location = new System.Drawing.Point(30, 110);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(164, 104);
+            this.listBox1.Size = new System.Drawing.Size(557, 160);
             this.listBox1.TabIndex = 1;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(254, 169);
+            this.textBox1.Location = new System.Drawing.Point(169, 75);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(134, 26);
+            this.textBox1.Size = new System.Drawing.Size(91, 20);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 50);
+            this.label1.Location = new System.Drawing.Point(217, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(508, 169);
+            this.textBox2.Location = new System.Drawing.Point(339, 75);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(137, 26);
+            this.textBox2.Size = new System.Drawing.Size(93, 20);
             this.textBox2.TabIndex = 4;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(254, 127);
+            this.label2.Location = new System.Drawing.Point(170, 60);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(150, 20);
+            this.label2.Size = new System.Drawing.Size(90, 13);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Wpisz nazwę waluty";
+            this.label2.Text = "Wpisz kod waluty";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(504, 127);
+            this.label3.Location = new System.Drawing.Point(336, 60);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(224, 20);
+            this.label3.Size = new System.Drawing.Size(137, 13);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Wpisz numer tabeli: (a, b lub c)";
+            this.label3.Text = "Wpisz typ tabeli: (a, b lub c)";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // StudentAPI
+            // label4
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Menu;
+            this.label4.Location = new System.Drawing.Point(256, 9);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "----------";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // CurrencyAPI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(623, 281);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
@@ -113,8 +134,9 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
-            this.Name = "StudentAPI";
-            this.Text = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "CurrencyAPI";
+            this.Text = "KURSY WALUT";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,10 +147,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
---
+++ WindowsFormsApp1/CurrencyAPI.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Data.Entity;
+using System.Runtime.Remoting.Contexts;
+using System.Security.Cryptography;
+using System.Runtime.ConstrainedExecution;
+
+namespace WindowsFormsApp1
+{
+    public partial class CurrencyAPI : Form
+    {
+        private Bank bank;
+
+        public CurrencyAPI()
+        {
+            InitializeComponent();
+            bank = new Bank();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string json = await downloadData();
+            Currency cur = JsonConvert.DeserializeObject<Currency>(json);
+            if (cur.table == "C")
+               label4.Text = cur.currency + "\nKurs kupna: " + cur.rates[0].bid.ToString() + " PLN  " + "\nKurs sprzedaży: " + cur.rates[0].ask.ToString() + " PLN";
+            else
+                label4.Text = cur.currency + "\nKurs średni: " + cur.rates[0].mid.ToString() + " PLN";
+            listBox1.Items.Add(cur);
+
+          //bank.CurrencyBoard.Add(new Currency() { table = cur.table, currency = cur.currency, code = cur.code, rates = cur.rates });
+            //bank.SaveChanges();
+            //var rates = bank.CurrencyBoard.ToList();
+            //foreach (var rate in rates)
+           //istBox1.Items.Add(rate);             //NIE DZIALA 
+
+        }
+
+        private async Task<string> downloadData()
+        {
+            string code = textBox1.Text;
+            string table = textBox2.Text;
+            HttpClient client = new HttpClient();
+            string call = "http://api.nbp.pl/api/exchangerates/rates/" + table + "/" + code + "/?format=json";
+            string json = await client.GetStringAsync(call);
+            return json;
+        }
+    }
+}
